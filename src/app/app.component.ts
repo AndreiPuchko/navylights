ac41217cb472424bd24d3daf9b36a32931066dcd
Andrei Puchko <andrei.puchko@gmx.de>
2021-08-17 23:09:06 +0200

    class Light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
+import { NumberSymbol } from '@angular/common';
 import { Component,ViewChild,ElementRef } from '@angular/core';
+
+const shortBlinkTime=1000;
+const longBlinkTime=2000;
+export class Light {
+  public color: string;
+  public timeOn: number;
+  public timeOff: number;
+  public repeatCount: number;
+
+  constructor(color:string,timeOn:number,timeOff:number,repeat:number ) {
+    this.color=color;
+    this.timeOn=timeOn;
+    this.timeOff=timeOff;
+    this.repeatCount=repeat;
+    }
+ }
 
 @Component({
   selector: 'app-root',
@@ -7,7 +24,6 @@ import { Component,ViewChild,ElementRef } from '@angular/core';
 })
 
 export class AppComponent {
-  
   title = 'navylights';
   navytext: string = 'Q(6)LFL';
   @ViewChild('navylight') navylight!: ElementRef<HTMLInputElement>;
@@ -17,13 +33,22 @@ export class AppComponent {
   }
   
   async onShow (){
-    for (let i = 0; i < 6; i++) 
-    {
-      await this.blink("white",500);
-      await this.blink("black",500);
+    let serie : Light[]=[];
+    serie.push(new Light("white",shortBlinkTime,shortBlinkTime,6));
+    serie.push(new Light("white",longBlinkTime,0,1));
+    // serie.push(new Light("yellow",shortBlinkTime,shortBlinkTime,2));
+    for (let x=0; x<serie.length;x++){
+      await this.show(serie[x]);
     }
-    await this.blink("white",2500);
     await this.blink("black");
+  }
+
+  async show (_light:Light){
+    for (let i = 0; i < _light.repeatCount; i++)
+    {
+      await this.blink(_light.color,_light.timeOn);
+      await this.blink("black",_light.timeOff);
+    }
   }
 
   async blink(color:string, delay=0){
